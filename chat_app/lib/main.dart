5a13410692cba4a1b802061db274692899166b29
Ahmed Mostafa <48592135+AhmedMostafaMohamed@users.noreply.github.com>
2023-02-16 12:35:10 +0200

    Revert "we're able to get data from the firestore database"

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'firebase_options.dart';
-
-void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
-  final ins = FirebaseFirestore.instance;
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +14,6 @@ class ChatScreen extends StatelessWidget {
                 padding: EdgeInsets.all(8.0),
                 child: Text('this works!'),
               )),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (() {
-          ins
-              .collection('chats/G70r5r6DoEM8PjkHHNCI/messages')
-              .snapshots()
-              .listen((event) {
-            print(event);
-          });
-        }),
-      ),
     );
   }
 }
